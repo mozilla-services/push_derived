33b34e147b93fe9848286ccb0d64b0c5c2a1155d
Rob Miller <rob@kalistra.com>
2016-05-04 17:38:30 -0700

    Invoke push_metrics w the right subcommand.

--- push_load.sh
+++ push_load.sh
@@ -31,4 +31,4 @@ sudo python setup.py develop
 ROLLUP_DB_STRING=postgresql+psycopg2://$DB_USER:$DB_PW@$DB_HOST:$DB_PORT/$DB_NAME
 ROLLUP_TABLE_PREFIX=webpush_endpoint_requests
 ROLLUP_DAYS_AGO=1
-push_metrics
+push_metrics process_logs
